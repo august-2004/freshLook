2e5cf1247151a805edc46df14d3d84d653ad5a3c
Vanjinathan <115717134+august-2004@users.noreply.github.com>
2024-02-10 15:34:36 +0530

    the clock works now

--- tryingout/scripts.js
+++ tryingout/scripts.js
@@ -1,3 +1,5 @@
+//tasks
+
 let tasksList =[];
 let taskInput = document.querySelector(".taskinp");
 let taskDiv = document.querySelector(".tasks");
@@ -6,9 +8,22 @@ taskAdd.onclick = taskmaker;
 function taskmaker(){
     const node = document.createElement("li");
     node.innerHTML=taskInput.value;
+    tasksList.push(taskInput.value);
     taskDiv.appendChild(node);
     taskInput.value="";
     console.log("working");
-
 }
 
+
+
+//time 
+
+let time= document.querySelector(".nowtime");
+function timeUpdater (){
+const now = new Date();
+let hour = now.getHours();
+let min = now.getMinutes();
+
+time.innerHTML=hour +":" +min;
+}
+setInterval(timeUpdater, 1000);
