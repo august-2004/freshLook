3b2766f06007c22286eaa04b483daa47faab7a92
Vanjinathan <115717134+august-2004@users.noreply.github.com>
2024-02-10 16:30:36 +0530

    Changed to 12 hour format and also made it not possible to enter empty tasks

--- tryingout/scripts.js
+++ tryingout/scripts.js
@@ -6,12 +6,13 @@ let taskDiv = document.querySelector(".tasks");
 let taskAdd = document.querySelector(".taskbutton");
 taskAdd.onclick = taskmaker;
 function taskmaker(){
+    if (taskInput.value != ""){
     const node = document.createElement("li");
     node.innerHTML=taskInput.value;
     tasksList.push(taskInput.value);
     taskDiv.appendChild(node);
     taskInput.value="";
-    console.log("working");
+    console.log("working");}
 }
 
 
@@ -23,6 +24,11 @@ function timeUpdater (){
 const now = new Date();
 let hour = now.getHours();
 let min = now.getMinutes();
+
+if (hour >12)
+{
+    hour-=12;
+}
 
 time.innerHTML=hour +":" +min;
 }
